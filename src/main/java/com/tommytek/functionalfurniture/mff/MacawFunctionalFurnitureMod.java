f2cfc6bde2b8e2086179b0981408ea3b6ccb8b65
Tommy <rsslone1120@gmail.com>
2026-04-24 02:54:13 -0400

    Workspace Setup

---
+++ src/main/java/com/tommytek/functionalfurniture/mff/MacawFunctionalFurnitureMod.java
@@ -0,0 +1,19 @@
+package com.tommytek.functionalfurniture.mff;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION,
+        dependencies = "required-after:mcwfurniture")
+public class MacawFunctionalFurnitureMod {
+
+    public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        LOGGER.info("{} initializing...", Tags.MOD_NAME);
+    }
+
+}
