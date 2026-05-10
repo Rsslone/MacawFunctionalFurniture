64a194f9387b7b2856239e6d2b9f3bd321bcbf82
Tommy <rsslone1120@gmail.com>
2026-05-09 22:41:31 -0400

    Fix missing item tooltips

--- src/main/java/com/tommytek/mcwfuncfurn/gui/FurnitureGui.java
+++ src/main/java/com/tommytek/mcwfuncfurn/gui/FurnitureGui.java
@@ -30,6 +30,13 @@ public class FurnitureGui extends GuiContainer {
     }
 
     @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        this.renderHoveredToolTip(mouseX, mouseY);
+    }
+
+    @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
         fontRenderer.drawString(containerName, 8, 6, 0x404040);
         fontRenderer.drawString(
